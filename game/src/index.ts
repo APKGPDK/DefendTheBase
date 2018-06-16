2b09235f8bec827b4269d56944de8e17029c2b64
Antoni Pelka <kontakt@antonipelka.com>
2018-06-16 22:45:23 +0200

    hud

--- game/src/index.ts
+++ game/src/index.ts
@@ -1,13 +1,15 @@
-import './style.css'
 import Game from './Engine/Game';
-import GameScene from './Scenes/GameScene';
-import MovingSystem from './Systems/MovingSystem';
-import EnemySystem from './Systems/EnemySystem';
 import './polyfills.ts';
+import GameScene from './Scenes/GameScene';
+import './style.css';
+import EnemySystem from './Systems/EnemySystem';
+import HUDSystem from './Systems/HUDSystem';
+import MovingSystem from './Systems/MovingSystem';
 
 const canvas = <HTMLCanvasElement>document.getElementById('game')
 
 const game = new Game(canvas)
 game.registerSystem(MovingSystem)
 game.registerSystem(EnemySystem)
+game.registerSystem(HUDSystem)
 game.start(GameScene)
